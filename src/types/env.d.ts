2d8dc3ba461f87dc55020a6323611ebeef50fc54
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-02-27 22:07:21 +0600

    fix(types): remove unneeded typedefs for env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,7 +3,6 @@ namespace NodeJS {
         BOT_TOKEN: string;
         CONFIG_FILE_PATH?: string;
         NO_REGISTER_GLOBAL_COMMANDS?: string;
-        DB_URL: string;
         DATA_FILE_PATH?: string;
     }
 }
